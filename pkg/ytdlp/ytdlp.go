c3484da2fcba7b9c9f6c7fe7698b8d41e8fea9e4
Franciszek <fr.strzezek@gmail.com>
2025-10-15 16:35:50 +0200

    something ¯\_(ツ)_/¯

--- pkg/ytdlp/ytdlp.go
+++ pkg/ytdlp/ytdlp.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"io"
 	"net/http"
 	"net/url"
@@ -35,7 +36,7 @@ const (
 	printAfterMove = "after_move:%(.{title,filepath,ext,original_url})j"
 )
 
-var playlistInfoCMD, videoDownloadCMD, audioDownloadCMD []string
+var playlistInfoCMD, videoDownloadCMD, videoDownloadSmallCMD, audioDownloadCMD []string
 
 func init() {
 	go func() {
@@ -54,6 +55,14 @@ func init() {
 			"--print", printAfterMove,
 			"-f",
 			"bestvideo+bestaudio/best",
+		}
+		videoDownloadSmallCMD = []string{
+			"--no-simulate",
+			"--cookies", filepath.Join(must(os.Getwd()), "prod-data", config.COOKIE_PATH),
+			"--print", printAfterMove,
+			"-f",
+			`bv*[filesize<8M]+ba[filesize<2M]/b[filesize<10M]/bv*[height<=720]+ba/b[height<=720]/b`,
+			"--format-sort", "codec:av01,+fps:30",
 		}
 
 		playlistInfoCMD = []string{
@@ -138,6 +147,41 @@ func (YTDLP) DownloadVideo(ctx context.Context, link string) (VideoMeta, error) 
 	return meta, nil
 }
 
+func (YTDLP) DownloadVideoSmall(ctx context.Context, link string) (VideoMeta, error) {
+	tmpDir, ok := ctx.Value(config.DirKey).(string)
+	if !ok || len(tmpDir) == 0 {
+		return VideoMeta{}, errors.New("nie dałeś temp dira debilu")
+	}
+
+	link, err := parseSpecialLinks(link)
+	if err != nil {
+		return VideoMeta{}, errors.Join(errors.New("Special Links Parser"), err)
+	}
+
+	cmd := exec.Command(ytdlp, append(videoDownloadSmallCMD, link)...)
+	cmd.Dir = string(tmpDir)
+
+	stdout := &bytes.Buffer{}
+	stderr := &bytes.Buffer{}
+	cmd.Stdout = stdout
+	cmd.Stderr = stderr
+
+	var meta VideoMeta
+	log.Trace("DownloadVideo", log.String("cmd", cmd.String()), log.String("link", link))
+	err = cmd.Run()
+	if err != nil {
+		b, _ := io.ReadAll(stderr)
+		return meta, errors.Join(ErrFailedToDownload, errors.New(string(b)))
+	}
+
+	err = json.NewDecoder(stdout).Decode(&meta)
+	if err != nil {
+		return meta, err
+	}
+
+	return meta, nil
+}
+
 type PlaylistMeta struct {
 	ID         string `json:"id"`
 	Title      string `json:"title"`
@@ -178,14 +222,14 @@ func (YTDLP) PlaylistInfo(link string) (PlaylistMeta, error) {
 func parseSpecialLinks(rawUrl string) (string, error) {
 	u, err := url.Parse(rawUrl)
 	if err != nil {
-		return rawUrl, errors.New("Error parsing url")
+		return rawUrl, fmt.Errorf("Error parsing url(%s): %w", rawUrl, err)
 	}
 	if !strings.HasSuffix(u.Host, "jbzd.com.pl") {
 		return rawUrl, nil
 	}
 	resp, err := http.Head(rawUrl)
 	if err != nil {
-		return rawUrl, errors.New("Error making web (HEAD) request url")
+		return rawUrl, fmt.Errorf("Error making web (HEAD) request url(%s): %w", rawUrl, err)
 	}
 	defer resp.Body.Close()
 	contentType := resp.Header["Content-Type"][0]
@@ -194,21 +238,21 @@ func parseSpecialLinks(rawUrl string) (string, error) {
 	}
 	resp, err = http.Get(rawUrl)
 	if err != nil {
-		return rawUrl, errors.New("Error making web (GET) request url")
+		return rawUrl, fmt.Errorf("Error making web (GET) request url(%s): %w", rawUrl, err)
 	}
 	defer resp.Body.Close()
 
 	doc, err := html.Parse(resp.Body)
 	if err != nil {
-		return rawUrl, errors.New("Error parsing body")
+		return rawUrl, fmt.Errorf("Error parsing body: %w", err)
 	}
 	for n := range doc.Descendants() {
-
 		if n.Type == html.ElementNode && n.Data == "videoplyr" {
 			for _, a := range n.Attr {
-				if a.Key == "video_url" {
-					return a.Val, nil
+				if a.Key != "video_url" {
+					continue
 				}
+				return a.Val, nil
 			}
 		}
 	}
